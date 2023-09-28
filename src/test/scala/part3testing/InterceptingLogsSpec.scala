a41e7cf49c71ee004d64591551bbe4681fabfe9a
huangh <hsl4125@outlook.com>
2023-09-28 12:02:20 +0800

    intercepting logs

---
+++ src/test/scala/part3testing/InterceptingLogsSpec.scala
@@ -0,0 +1,120 @@
+package part3testing
+
+import akka.actor.testkit.typed.scaladsl.LoggingTestKit
+import akka.actor.testkit.typed.scaladsl.ScalaTestWithActorTestKit
+import akka.actor.typed.scaladsl.Behaviors
+import akka.actor.typed.{ActorRef, Behavior}
+import org.scalatest.wordspec.AnyWordSpecLike
+
+
+class InterceptingLogsSpec extends ScalaTestWithActorTestKit with AnyWordSpecLike {
+
+  import InterceptingLogsSpec._
+
+  val item = "Rock the JVM Akka course"
+  val creditCard = "1234-1234-1234-1234"  // valid card
+  val invalidCard = "0000-0000-0000-0000"
+
+  "A checkout flow" should {
+    "correctly dispatch an order for valid credit card" in {
+      LoggingTestKit.info("Order")  // filter log message of level INFO which the string "Order"
+        .withMessageRegex(s"Order [0-9]+ for $item has been dispatched.")
+        .withOccurrences(1)
+        .expect {
+          // scenario under test
+          val checkoutActor = testKit.spawn(CheckoutActor())
+          checkoutActor ! Checkout(item, creditCard)
+        }
+    }
+
+    "freak out if the payment is declined" in {
+      LoggingTestKit.error[RuntimeException]
+        .withOccurrences(1)
+        .expect {
+          val checkoutActor = testKit.spawn(CheckoutActor())
+          checkoutActor ! Checkout(item, invalidCard)
+        }
+    }
+  }
+}
+
+object InterceptingLogsSpec {
+
+  // payment system
+  /*
+    checkout actor -> payment manager
+                   <-
+
+                   -> fulfillment manager
+                   <-
+   */
+  trait PaymentProtocol
+  case class Checkout(item: String, creditCard: String) extends PaymentProtocol
+  case class AuthorizeCar(creditCard: String, replyTo: ActorRef[PaymentProtocol]) extends PaymentProtocol
+  case object PaymentAccepted extends PaymentProtocol
+  case object PaymentDeclined extends  PaymentProtocol
+  case class DispatchOrder(item: String, replyTo: ActorRef[PaymentProtocol]) extends PaymentProtocol
+  case object OrderConfirmed extends PaymentProtocol
+
+  object CheckoutActor {
+    def apply(): Behavior[PaymentProtocol] = Behaviors.setup { context =>
+      val paymentManager = context.spawn(PaymentManager(), "PaymentManager")
+      val fulfillmentManager = context.spawn(FulfillmentManager(), "FulfillmentManager")
+
+      def awaitingCheckout(): Behavior[PaymentProtocol] = Behaviors.receiveMessage {
+        case Checkout(item, card) =>
+          context.log.info(s"Received order for item $item")
+          paymentManager ! AuthorizeCar(card, context.self)
+          pendingPayment(item)
+        case _ => Behaviors.same
+      }
+
+      def pendingPayment(item: String): Behavior[PaymentProtocol] = Behaviors.receiveMessage {
+        case PaymentAccepted =>
+          fulfillmentManager ! DispatchOrder(item, context.self)
+          pendingDispatch(item)
+        case PaymentDeclined =>
+          throw new RuntimeException("Cannot handle invalid payments")  // by design
+        case _ => Behaviors.same
+      }
+
+      def pendingDispatch(item: String): Behavior[PaymentProtocol] = Behaviors.receiveMessage {
+        case OrderConfirmed =>
+          context.log.info("Dispatch confirmed")
+          awaitingCheckout()
+      }
+
+      // the first state
+      awaitingCheckout()
+    }
+  }
+
+  object PaymentManager {
+    def apply(): Behavior[PaymentProtocol] = Behaviors.receiveMessage {
+      case AuthorizeCar(card, replyTo) =>
+        if (card.startsWith("0")) {
+          // validation logic
+          replyTo ! PaymentDeclined
+        } else {
+          replyTo ! PaymentAccepted
+        }
+        Behaviors.same
+      case _ => Behaviors.same
+    }
+  }
+
+  object FulfillmentManager {
+    def apply(): Behavior[PaymentProtocol] = active(43)
+
+    def active(orderId: Int): Behavior[PaymentProtocol] = Behaviors.receive { (context, message) =>
+      message match {
+        case DispatchOrder(item, replyTo) =>
+          context.log.info(s"Order $orderId for $item has been dispatched.")
+          replyTo ! OrderConfirmed
+          active(orderId + 1)
+        case _ => Behaviors.same
+      }
+    }
+  }
+  
+}
